4ce84f5c201bc9bd5c2e70d5780b05dc3b78bcf1
Brewyard <to.38@live.fr>
2020-05-13 23:46:21 +0200

    navig js sans chargement nouvelle page et animations et navbar active

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-// load the navbar
-$(function(){
-  $('#navBarPlaceholder').load('./components/navbar.html');
-  $('#footerPlaceholder').load('./components/footer.html');
-});
+// $(function(){
+//   $('navbar-nav nav-link').preventDefault();
+// });
+
+function loadMethode(navLink) {
+  changeActive(navLink);
+  $('#centerPlaceholder').load('./components/methode.html').hide().fadeIn('slow', retournerDebutScroll);
+};
+
+function loadQuisuisje(navLink) {
+  changeActive(navLink);
+  $('#centerPlaceholder').load('./components/quisuisje.html').hide().fadeIn('slow', retournerDebutScroll);
+};
+
+function changeActive(navLink) {
+  $('.navbar-nav .active').removeClass('active');
+  $(navLink).addClass('active');
+}
+
+function retournerDebutScroll() {
+  $('html, body').animate({
+      scrollTop: $("#centerPlaceholder").offset().top - $('.navbar').outerHeight()
+  }, 200);
+}
